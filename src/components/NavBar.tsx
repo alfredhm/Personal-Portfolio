f64dc432a22ada99c559cdfc9a9cbdf03692a98e
alfredhm <alfredhmcode@gmail.com>
2023-08-14 19:49:00 -0500

    initial:

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,55 @@
+import LogoIcon from "../assets/logo.png";
+import AboutIcon from "../assets/about.png";
+import ExperienceIcon from "../assets/work.png";
+import ProjectsIcon from "../assets/projects.png";
+import ContactIcon from "../assets/contact.png";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav className="header-flex">
+        <img
+          onClick={() => location.reload()}
+          className="logo"
+          src={LogoIcon}
+        />
+        <ol className="links">
+          <li>
+            <a className="link">
+              <div className="link-details">
+                <img className="icon" src={AboutIcon} />
+                <span id="link-title">About</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a className="link">
+              <div className="link-details">
+                <img className="icon" src={ExperienceIcon} />
+                <span id="link-title">Experience</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a className="link">
+              <div className="link-details">
+                <img className="icon" src={ProjectsIcon} />
+                <span id="link-title">Projects</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a className="link">
+              <div className="link-details">
+                <img className="icon" src={ContactIcon} />
+                <span id="link-title">Contact</span>
+              </div>
+            </a>
+          </li>
+        </ol>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
