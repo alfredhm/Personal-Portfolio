8be0795774286b92e6a5c10ab9c539d9a6e36a86
alfredhm <alfredhmcode@gmail.com>
2023-08-25 02:32:15 -0500

    x

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -26,7 +26,7 @@ const NavMenu = () => {
         <Menu>
           <MenuButton
             as={Button}
-            style={{ backgroundColor: "#44576D" }}
+            style={{ color: "#44576D" }}
             _focus={{ boxShadow: "none" }}
           >
             <HiMenuAlt3 size={30} />
